a35bed7b5057779e02453b29f02a968d50834a5d
nelson <nelson.chibukasia@globallinkplus.com>
2024-03-31 01:43:55 +0300

    root

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('<h2>Home route</h2>');
 });
 
 module.exports = router;
