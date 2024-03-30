de67cdfd065ea0e10058da83212096c01b26abf0
nelson <nelson.chibukasia@globallinkplus.com>
2024-03-31 01:52:42 +0300

    fix render issue

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -34,7 +33,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 const PORT = process.env.PORT || 3000;
